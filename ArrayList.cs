36b6c4e57cd22e0fb3c423c86fb117c15a54760b
Brami <EYBR@ferring.com>
2020-02-27 08:59:19 +0200

    Add project files.

---
+++ ArrayList.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L20200226_List
+{
+    public class ArrayList
+    {
+        private int[] arr;
+        private int count;
+
+
+        public ArrayList()
+        {
+            this.arr = new int[10];
+            this.count = 0;
+        }
+        public string ToString()
+        {
+            string List="";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                List += arr[i] + (i == arr.Length - 1 ? "" : ",");
+            }
+            return List;
+            
+        }
+        public bool Equals(ArrayList obj)
+        {
+            return arr.Equals(obj);
+        }
+        public void Add(int x)
+        {
+            if (count == arr.Length)
+            {
+                /*
+                 * זמן ריצה O(n*n)
+                int[] temp = new int[count + 1]; 
+                */
+                // זמן ריצה O(n)
+                int[] temp = new int[count * 2];
+                for (int i = 0; i < arr.Length; i++) 
+                {
+                    temp[i] = arr[i];
+                }
+                this.arr = temp;
+            }
+            arr[count++] = x;
+
+        }
+        public void Add(int index, int x)
+        {
+            this.arr[index] = x;
+        }
+        public int Get(int index)
+        {
+            return this.arr[index];
+        }
+        /// <summary>
+        /// מקבל ערך ומכניס אותו למקום
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="x"></param>
+        public void Set(int index, int x)
+        {
+            this.arr[index] = x;
+        }
+        /// <summary>
+        /// מוחק איבר מהמקום ה-INDEX
+        /// </summary>
+        /// <param name="index"></param>
+        public void Remove(int index)
+        {
+
+        }
+        /// <summary>
+        /// מחזיר כמה איברים יש ברשימה
+        /// </summary>
+        /// <returns></returns>
+        public int Count()
+        {
+
+            return arr.Length;
+        }
+        /// <summary>
+        /// החזיר ערך לפי המיקום של האיבר
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public int IndexOf(int x)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != x)
+                    continue;
+                return i;
+            }
+            return 0;
+        }
+        /// <summary>
+        /// מחזיר את הרשימה כמערך
+        /// </summary>
+        /// <returns></returns>
+        public int[] ToArray()
+        {
+            return arr;
+        }
+        /// <summary>
+        /// איפוס של הרשימה
+        /// </summary>
+        public void Clear()
+        {
+            new ArrayList();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L20200226_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(25);
+            arrayList.Add(10);
+            arrayList.Add(14);
+            arrayList.Add(3);
+            arrayList.Add(400);
+
+            int n = 100;
+            for (int j = 0; j < n; j++)
+            {
+                arrayList.Add(j + 1000);
+            }
+
+            Console.WriteLine("{" + arrayList.ToString() + "}");
+
+            Console.WriteLine($"The index is : { arrayList.IndexOf(14)}");
+
+
+
+
+
+            ////אפשרות לרוקן את הרשימה
+            //while(arrayList.Count() > 0)
+            //{
+            //    arrayList.Remove(arrayList.Count() - 1);
+            //}
+
+            
+            Console.ReadLine();
+        }
+    }
+}
